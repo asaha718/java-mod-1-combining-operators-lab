0747185828faf489cdd504cec2d31c23d7812e21
Anug Saha <anugsaha@gmail.com>
2022-06-23 11:27:16 -0400

    creating a truth table with boolean combinations

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        // your code here
+        System.out.println( "truth && truth = " + (true && true));
+        System.out.println( "truth && false = " + (true && false));
+        System.out.println( "false && false = " + (false && false));
+        System.out.println( "false && truth = " + (false && true));
     }
 }
