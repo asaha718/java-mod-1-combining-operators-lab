9570bce035088cfda64a254c6c746fd3660e21b3
Anug Saha <anugsaha@gmail.com>
2022-06-23 11:32:10 -0400

    truth table for or operator as well. code comments

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println( "truth && truth = " + (true && true));
-        System.out.println( "truth && false = " + (true && false));
-        System.out.println( "false && false = " + (false && false));
-        System.out.println( "false && truth = " + (false && true));
+
+        System.out.println( "Truth table for && operator :");
+        System.out.println( "truth && truth = " + (true && true)); // true
+        System.out.println( "truth && false = " + (true && false)); // false
+        System.out.println( "false && false = " + (false && false)); // false
+        System.out.println( "false && truth = " + (false && true)); // false
+
+        System.out.println( "Truth table for || operator :");
+        System.out.println( "truth || truth = " + (true || true)); // true
+        System.out.println( "truth || false = " + (true || false)); // true
+        System.out.println( "false || false = " + (false || false)); // false
+        System.out.println( "false || truth = " + (false || true)); // true
+
     }
 }
